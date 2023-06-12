56de7678a20b8f4f1c13d67e3a8975811b0982de
Raman Arunachalam <raman_arunachalam@yahoo.com>
2023-06-12 14:28:22 +0530

    Carnatic Updates

--- Carnatic/js/transliterate.js
+++ Carnatic/js/transliterate.js
@@ -110,7 +110,7 @@ function set_tamil_regex_list() {
 
 function get_map_data(key) {
     const lang_map_data = window.LANG_MAPS;
-    return (lang_map_data != undefined) ? lang_map_data.get(key) : Object();
+    return (lang_map_data !== undefined) ? lang_map_data.get(key) : Object();
 }
 
 function transliterator_lang_init(lang) {
@@ -121,15 +121,15 @@ function transliterator_lang_init(lang) {
 
 function get_transliterator_text(out_lang, data) {
     const lang_map_data = window.LANG_MAPS;
-    if (lang_map_data == undefined) return data;
+    if (lang_map_data === undefined) return data;
     out_lang = out_lang.toLowerCase();
     let result = data;
-    if (out_lang == 'tamil') {
+    if (out_lang === 'tamil') {
         result = apply_regex(window.HKT_REGEX_OBJ_LIST, result);
     } else {
         result = result.replace('.N', 'n');
     }
-    if (out_lang != 'english') {
+    if (out_lang !== 'english') {
         const lang_map = lang_map_data.get(out_lang);
         const lang_freq = lang_map_data.get('hk_tamil_freq');
         result = transliterate_map_freq_text(lang_map, lang_map_data.get('hk_tamil_length'), lang_freq, result);
@@ -162,12 +162,12 @@ function render_keys(lang_dict) {
         let key = key_list[k];
         basic_list = lang_dict[key];
         for (let i = 0; i < basic_list.length; i++) {
-            if (j > 0 && (j % ROW_SIZE) == 0) {
+            if (j > 0 && (j % ROW_SIZE) === 0) {
                 row_list.push({ 'col' : row });
                 row = []
             }
             let c = basic_list[i];
-            c = c != '.' ? c : ' ';
+            c = c !== '.' ? c : ' ';
             const info = { 'N' : c, 'K' : c, 'T' : 'key', 'I' : col_id };
             row.push(info);
             info_list.push(info);
@@ -179,7 +179,7 @@ function render_keys(lang_dict) {
     for (let i = 0; i < punctuation_list.length; i++) {
         const c = punctuation_list[i];
         const info = { 'N' : c, 'K' : c, 'T' : 'key', 'I' : col_id };
-        if (j > 0 && (j % ROW_SIZE) == 0) {
+        if (j > 0 && (j % ROW_SIZE) === 0) {
             row_list.push({ 'col' : row });
             row = []
         }
@@ -192,7 +192,7 @@ function render_keys(lang_dict) {
     const key_name_list = [ 'vowel reset', 'backspace', 'enter' ];
     for (let i = 0; i < icon_list.length; i++) {
         const icon = icon_list[i];
-        const img_str = `<img class="ICON" src="icons/${icon}.svg" onclick="on_key_click()">`;
+        const img_str = `<i class="bi bi-${icon} ICON_FONT" onclick="on_key_click()"></i>`;
         const info = { 'N' : img_str, 'A' : key_name_list[i], 'K' : icon, 'T' : 'icon', 'I' : col_id };
         row.push(info);
         info_list.push(info);
@@ -211,12 +211,12 @@ function render_keys(lang_dict) {
         const key = key_list[k];
         basic_list = lang_dict[key];
         for (let i = 0; i < basic_list.length; i++) {
-            if (j > 0 && (j % ROW_SIZE) == 0) {
+            if (j > 0 && (j % ROW_SIZE) === 0) {
                 row_list.push({ 'col' : row });
                 row = []
             }
             let c = basic_list[i];
-            c = c != '.' ? c : ' ';
+            c = c !== '.' ? c : ' ';
             const info = { 'N' : c, 'K' : c, 'T' : 'key', 'I' : col_id };
             row.push(info);
             info_list.push(info);
@@ -233,7 +233,7 @@ function replace_keys(key_dict, vowel_size, key) {
     const combo_list = window.script_combo_list;
     for (let i = 0; i < vowel_size; i++) {
         const c_key = combo_list[i];
-        const c = (key == '' || c_key == 0) ? key_dict[i]['N'] : key + c_key;
+        const c = (key === '' || c_key === 0) ? key_dict[i]['N'] : key + c_key;
         const col_id = 'key_' + (i + 1);
         plain_set_html_text(col_id, c);
     }
@@ -243,7 +243,7 @@ function on_key_click() {
    const lang_dict = window.script_lang_dict;
    let text = document.getElementById('SEARCH_WORD').value;
    let element = event.srcElement;
-   if (element.tagName == 'IMG') {
+   if (element.tagName === 'I') {
        event.stopPropagation();
        element = element.parentElement;
    }
@@ -254,19 +254,19 @@ function on_key_click() {
    const f = c.charCodeAt(0);
    let r_key;
    // console.log(`c ${c} f ${f} id ${id} nid ${nid}`);
-   if (id == lang_dict['vowel reset']) {
+   if (id === lang_dict['vowel reset']) {
        r_key = '';
-   } else if (id == lang_dict['backspace']) {
+   } else if (id === lang_dict['backspace']) {
        if (text.length > 0) {
            text = text.slice(0, text.length - 1);
        }
-   } else if (id == lang_dict['enter']) {
+   } else if (id === lang_dict['enter']) {
        load_search_data();
    } else if (lang_dict['consonant'].includes(s) || lang_dict['middle'].includes(s)) {
        const pos = c.length - 1;
        r_key = c;
        const l = text[text.length - pos];
-       if (l != undefined && l.charCodeAt(0) == f) {
+       if (l !== undefined && l.charCodeAt(0) === f) {
            const p = text.length - 1;
            if (superscript_code_list.has(text[p])) {
                text = text.slice(0, p) + c[pos - 1] + c[pos];
@@ -283,7 +283,7 @@ function on_key_click() {
    } else {
        text += c;
    }
-   if (r_key != undefined) {
+   if (r_key !== undefined) {
        replace_keys(window.input_key_dict, window.script_vowel_size, r_key);
    }
    document.getElementById('SEARCH_WORD').value = text;
@@ -299,7 +299,7 @@ function set_input_keyboard(lang) {
     for (let i = 0; i < info_list.length; i++) {
         const info_dict = info_list[i];
         const key_name = info_dict['A'];
-        if (key_name != undefined) {
+        if (key_name !== undefined) {
             lang_dict[key_name] = 'key_' + info_dict['I'];
         }
     } 
